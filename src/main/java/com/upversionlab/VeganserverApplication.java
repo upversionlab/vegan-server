736409374336a65cdc3c3fff7b91f8a0925be8c5
rborcat <rodrigoborcat@gmail.com>
2016-12-06 20:35:59 -0200

    initial commit

---
+++ src/main/java/com/upversionlab/VeganserverApplication.java
@@ -0,0 +1,12 @@
+package com.upversionlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeganserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeganserverApplication.class, args);
+	}
+}
