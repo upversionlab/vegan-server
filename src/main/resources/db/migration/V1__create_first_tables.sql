026b06c090bd93ef214b8d340fcd26db54900508
Vinicius Uzeda <vruzeda@gmail.com>
2017-03-11 09:45:35 -0300

    Using the vegan flags proprosed in the app's interface mockups.

--- src/main/resources/db/migration/V1__create_first_tables.sql
+++ src/main/resources/db/migration/V1__create_first_tables.sql
@@ -1,3 +1,13 @@
+CREATE TABLE VeganFlags (
+    id SERIAL PRIMARY KEY,
+    meat BOOLEAN,
+    egg BOOLEAN,
+    milk BOOLEAN,
+    honey BOOLEAN,
+    gluten BOOLEAN,
+    organic BOOLEAN
+);
+
 CREATE TABLE Certification (
     id SERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
@@ -8,7 +18,8 @@ CREATE TABLE Company (
     id SERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     logo_url VARCHAR(100),
-    vegan BOOLEAN,
+    animal_tests BOOLEAN,
+    animal_resources BOOLEAN,
     website VARCHAR(100)
 );
 
@@ -21,7 +32,7 @@ CREATE TABLE CompanyCertification (
 CREATE TABLE Ingredient (
     id SERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
-    vegan BOOLEAN
+    vegan_flags_id INTEGER REFERENCES VeganFlags (id)
 );
 
 CREATE TABLE Category (
@@ -37,7 +48,6 @@ CREATE TABLE Product (
     name VARCHAR(100) NOT NULL,
     company_id INTEGER NOT NULL REFERENCES Company (id),
     package_image_url VARCHAR(100),
-    vegan BOOLEAN,
     type ProductType
 );
 
@@ -51,7 +61,7 @@ CREATE TABLE ProductIngredient (
     id SERIAL PRIMARY KEY,
     product_id INTEGER NOT NULL REFERENCES Product (id),
     ingredient_id INTEGER NOT NULL REFERENCES Ingredient (id),
-    vegan BOOLEAN
+    vegan_flags_id INTEGER REFERENCES VeganFlags (id)
 );
 
 CREATE TABLE ProductCategory (
