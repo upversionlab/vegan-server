5c8d655545b7a02f263a779f5106388c9676d983
rborcat <rodrigoborcat@gmail.com>
2017-05-04 22:35:16 -0300

    created folder based on timestamp of the uploaded file

--- src/main/java/com/upversionlab/storage/FileSystemStorageService.java
+++ src/main/java/com/upversionlab/storage/FileSystemStorageService.java
@@ -9,30 +9,45 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 @Service
 public class FileSystemStorageService {
 
-    private final Path rootLocation;
+    private final String storageLocation;
 
     @Autowired
     public FileSystemStorageService(String storageLocation) {
-        this.rootLocation = Paths.get(storageLocation);
+        this.storageLocation = storageLocation;
     }
 
     public void storeAndroid(MultipartFile barcodeFile, MultipartFile logo, MultipartFile ingredientList, MultipartFile nutritionalFacts) {
-        doStore(barcodeFile, "barcode.jpg");
-        doStore(logo, "logo.jpg");
-        doStore(ingredientList, "ingredientList.jpg");
-        doStore(nutritionalFacts, "nutrictionalFacts.jpg");
+        Path rootLocation = createFolders();
+        doStore(rootLocation, barcodeFile, "barcode.jpg");
+        doStore(rootLocation, logo, "logo.jpg");
+        doStore(rootLocation, ingredientList, "ingredientList.jpg");
+        doStore(rootLocation, nutritionalFacts, "nutrictionalFacts.jpg");
     }
 
-    private void doStore(MultipartFile file, String filename) {
+    private Path createFolders() {
+        Long now = Calendar.getInstance().getTimeInMillis();
+        Path rootLocation = Paths.get(storageLocation, now.toString());
+        try {
+            Files.createDirectories(rootLocation);
+        } catch (IOException e) {
+            throw new StorageException("Could not create the storage: " + storageLocation + "/" + now.toString(), e);
+        }
+        return rootLocation;
+    }
+
+    private void doStore(Path rootLocation, MultipartFile file, String filename) {
         try {
             if (file.isEmpty()) {
                 throw new StorageException("Failed to store empty file " + file.getOriginalFilename());
             }
-            Files.copy(file.getInputStream(), this.rootLocation.resolve(filename));
+            Files.copy(file.getInputStream(), rootLocation.resolve(filename));
         } catch (IOException e) {
             throw new StorageException("Failed to store file " + file.getOriginalFilename(), e);
         }
